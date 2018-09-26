905e8563ec02e426310c6ce825db7af8460d7aae
thienth32 <thienth32@gmail.com>
2018-09-26 10:13:08 +0700

    complete admin layout

---
+++ admin/_share/footer.php
@@ -0,0 +1,9 @@
+<?php 
+	require_once '../commons/utils.php';
+ ?>
+<footer class="main-footer">
+<div class="pull-right hidden-xs">
+  <b>Version</b> 2.4.0
+</div>
+<strong>Copyright &copy; 2018 <a href="javascript:;">Pt13313</a>.</strong> All rights reserved.
+</footer>
